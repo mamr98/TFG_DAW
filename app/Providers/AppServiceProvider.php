92b236cf150b5e1d66e4e0a9f51e968a9903623b
mamr98 <milena.miguel.angel@gmail.com>
2025-03-25 22:33:44 +0100

    cambios en appserviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
