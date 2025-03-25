4f2e3474919ad5d50ff1759a93489dba601aa0c7
mamr98 <milena.miguel.angel@gmail.com>
2025-03-25 14:12:40 +0100

    vuelta de 0 a toMail

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -3,11 +3,9 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 
 class VerifyEmail extends Notification
 {
@@ -34,27 +32,11 @@ class VerifyEmail extends Notification
     /**
      * Get the mail representation of the notification.
      */
-    /* public function toMail(object $notifiable): MailMessage
+    public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
             ->line('The introduction to the notification.')
             ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    } */
-
-    public function toMail(object $notifiable): MailMessage
-    {
-        // Generar el enlace de verificación firmado con una expiración de 120 minutos
-        $url = URL::temporarySignedRoute(
-            'verification.verify', // Nombre de la ruta de verificación
-            Carbon::now()->addMinutes(120), // Tiempo de expiración (120 minutos)
-            ['id' => $notifiable->getKey(), 'hash' => sha1($notifiable->email)] // Pasar ID y hash
-        );
-
-        return (new MailMessage)
-            ->line('Please click the button below to verify your email address.')
-            ->action('Verify Email Address', $url) // Botón con el enlace de verificación
-            ->line('If you did not create an account, no further action is required.')
             ->line('Thank you for using our application!');
     }
 
