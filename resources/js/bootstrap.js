9871ca1961438fdbe97c5f842ed06d6c44ede51f
mamr98 <milena.miguel.angel@gmail.com>
2025-03-25 22:09:51 +0100

    le pasamos por la cabecera las cookies con axio

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
 
+axios.defaults.withCredentials = true; // ¡IMPORTANTE! Envía cookies (incluyendo XSRF-TOKEN)
+// Si falta withCredentials: true, las cookies no se enviarán y el CSRF fallará.
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
