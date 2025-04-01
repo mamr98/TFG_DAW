f3a0eff400303e2bacd8016bad826935b8330b63
mamr98 <milena.miguel.angel@gmail.com>
2025-04-01 09:05:58 +0200

    Cambios en el diseño del crud de profesores y alumnos

--- resources/js/Components/hooks/CrudUsuario.jsx
+++ resources/js/Components/hooks/CrudUsuario.jsx
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import PrimaryButton from "../PrimaryButton";
 
 function CrudUsuario() {
-
-
     return (
-        <div className="flex flex-col items-center justify-center p-6 bg-[#003049] shadow-md rounded-lg">
-            <h3 className="text-lg font-semibold text-white mb-4">CRUD Usuarios</h3>
+        <div className="flex p-6 bg-[#003049] shadow-md rounded-lg">
+            {/* Columna izquierda - Alumnos */}
+            <div className="w-1/2 pr-4"> {/* pr-4 para un poco de espacio entre columnas */}
+                <div className="flex flex-col items-center justify-center">
+                    <h3 className="text-lg font-semibold text-white mb-4">CRUD Alumnos</h3>
 
-            {/* Botón de crear */}
-            <PrimaryButton children={"Crear Usuario"} className="mt-4" />
+                    {/* Botón de crear */}
+                    <PrimaryButton children={"Crear Alumnos"} className="mt-4 w-full" />
 
-            {/* Botón de actualizar */}
-            <PrimaryButton children={"Actualizar Usuario"} className="mt-4" />
+                    {/* Botón de actualizar */}
+                    <PrimaryButton children={"Actualizar Alumnos"} className="mt-4 w-full" />
 
-            {/* Botón de borrar */}
-            <PrimaryButton children={"Borrar Usuario"} className="mt-4" />
+                    {/* Botón de borrar */}
+                    <PrimaryButton children={"Borrar Alumnos"} className="mt-4 mb-14 w-full" />
+                </div>
+            </div>
+
+            {/* Columna derecha - Profesores */}
+            <div className="w-1/2 pl-4"> {/* pl-4 para un poco de espacio entre columnas */}
+                <div className="flex flex-col items-center justify-center">
+                    <h3 className="text-lg font-semibold text-white mb-4">CRUD Profesores</h3>
+
+                    {/* Botón de crear */}
+                    <PrimaryButton children={"Crear Profesores"} className="mt-4 w-full" />
+
+                    {/* Botón de actualizar */}
+                    <PrimaryButton children={"Actualizar Profesores"} className="mt-4 w-full" />
+
+                    {/* Botón de borrar */}
+                    <PrimaryButton children={"Borrar Profesores"} className="mt-4 w-full" />
+                </div>
+            </div>
         </div>
     );
 }
