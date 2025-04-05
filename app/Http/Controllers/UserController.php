144fb42ac642db46dac04bef7d8f8262c1ac9f9b
Davpp24 <pelaezperezdavid@gmail.com>
2025-04-05 19:22:42 +0200

    crud hecho

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,6 +25,7 @@ class UserController extends Controller
      */
     public function createAlumno(Request $request)
     {
+        
         $user = new User();
         $user -> name = $request ->input('name');
         $user -> email = $request ->input('email');
