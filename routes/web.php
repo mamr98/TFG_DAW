777741efd64b7b5c7297c88db652a504867bc177
PabloTorr03 <pablogdlt03@gmail.com>
2025-04-10 17:16:34 +0200

    Comentario para public path

--- routes/web.php
+++ routes/web.php
@@ -135,7 +135,7 @@ Route::get('/test-textract', function () {
     try {
         $result = $client->analyzeDocument([
             'Document' => [
-                'Bytes' => file_get_contents('https://res.cloudinary.com/dpnclrw0v/image/upload/v1744297281/examenes/ubpqa7wfwynhlnnxxzfp.png')
+                'Bytes' => file_get_contents('https://res.cloudinary.com/dpnclrw0v/image/upload/v1744297281/examenes/ubpqa7wfwynhlnnxxzfp.png')//public_path('prueba2.png')
             ],
             'FeatureTypes' => ['TABLES'],
         ]);
