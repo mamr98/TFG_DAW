e302af47751f5d62e49521d5380502c744af9297
Marcoide120 <marcosgarciacasta5000@gmail.com>
2025-04-23 22:03:41 +0200

    fichero export modificado para que utilize una vista en vez de una coleccion

--- app/Exports/ExamenAlumnoExport.php
+++ app/Exports/ExamenAlumnoExport.php
@@ -4,14 +4,24 @@ namespace App\Exports;
 
 use App\Models\ExamenAlumno;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View; // Importar esta librería para utilizar una vista en vez de una colección
+use Maatwebsite\Excel\Concerns\FromView; // Importar esta librería para utilizar una vista en vez de una colección
 
-class ExamenAlumnoExport implements FromCollection
+class ExamenAlumnoExport implements FromView //FromCollection
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    /* public function collection()
     {
         return ExamenAlumno::all();
+    } */
+
+    public function view(): View
+    {
+        return view('(nombre de la vista)',[
+            'variable del foreach' => ExamenAlumno::all()
+        ]);
     }
+
 }
