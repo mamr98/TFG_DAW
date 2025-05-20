f95062adb02365c7e5aae4b1cfed22df085fb353
PabloTorr03 <126698648+PabloTorr03@users.noreply.github.com>
2025-05-20 13:08:41 +0200

    cierre etiqueta

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -220,6 +220,7 @@ export default function AuthenticatedLayout({ header, children }) {
                         >
                             Notas
                         </ResponsiveNavLink>
+                        </Can>
                         <Can permissions={["permisoprofesor"]}>
                             <ResponsiveNavLink
                                 href={route('mostrarAsignaturas')}
